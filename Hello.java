7994c9c9182941a303b3b3124955bceb3f563b37
sonam58 <sonamyadav.me08@gmail.com>
2021-11-17 21:19:34 +0530

    updated version

--- Hello.java
+++ Hello.java
@@ -2,8 +2,19 @@
 * Prasad Patil 11/10/21
 */
 
+/*For Pull process Adding logger in updated Git sample.
+* Sonam Yadav 17/11/21
+*/
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Hello{
+	
+	private static final Logger Log = LogManager.getLogger(Hello.class);
+	
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		logger.info("Sample info message");
+		logger.debug("Hello World");
 	}
 }
